c37fc76222dfed1ced8e338449e36d13871afbcb
alinesoglia <alinepsoglia@gmail.com>
2023-05-11 15:06:38 -0300

    criando primeiro componente com React

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Home from "./paginas/home/Home"
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <Home />
+  )
+}
+
+export default App
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Home.css";
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://content.invisioncic.com/v310067/monthly_2021_08/image.png.3f6bd59981ca4ec23cf019c2de47ef56.png" alt="Imagem da Tela Principal" className='img'/>
+        </>
+    );
+}
+
+export default Home;
