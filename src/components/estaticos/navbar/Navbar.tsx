7b84fa7421be5e9c54a98b46ed95dda9ada2303d
alinesoglia <alinepsoglia@gmail.com>
2023-05-16 14:16:21 -0300

    Criando o sistemas de rotas

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,44 +1,48 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Box, Grid} from '@material-ui/core';
+import "./Navbar.css"
+import {Link} from "react-router-dom"
 
 function Navbar() {
     return (
         <>
-            <AppBar position="static" style={{background: "#7B0D1E"}}>
+            <AppBar position="static" style={{background: "#0c0c0c"}}>
                 <Toolbar variant="dense">
                     <Grid container justifyContent = {'space-between'}>
-                    <Box style={{ cursor: "pointer" }} >
+                    <Box className='hover cursor'>
                         <Typography variant="h5" color="inherit">
                             BlogPessoal
                         </Typography>
                     </Box>
 
                     <Box display="flex" justifyContent="start">
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='hover cursor'>
                             <Typography variant="h6" color="inherit">
                                 Home
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='hover cursor'>
                             <Typography variant="h6" color="inherit">
                                 Postagens
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='hover cursor'>
                             <Typography variant="h6" color="inherit">
                                 Temas
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='hover cursor'>
                             <Typography variant="h6" color="inherit">
                                 Cadastrar tema
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Link to="/login" className="text-decorator-none">
+                        <Box mx={1} className='hover cursor'>
                             <Typography variant="h6" color="inherit">
                                 Logout
                             </Typography>
                         </Box>
+                        </Link>
                     </Box>
                     </Grid>
                 </Toolbar>
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -6,22 +6,22 @@ import { Grid, Box, Typography, Button } from "@material-ui/core"
 function Home() {
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#F8E5EE" }}>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "black", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "black", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>Expresse aqui os seus pensamentos e opiniões!</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
                         </Box>
-                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#9F2042", color: "white" }}>Ver Postagens</Button>
+                        <Button variant="outlined" className='postHover botao'>Ver Postagens</Button>
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
-                    <img src="https://contenthub-static.grammarly.com/blog/wp-content/uploads/2017/11/how-to-write-a-blog-post.jpeg" alt="" width="600px" height="400px" />
+                    <img src="https://img.freepik.com/free-vector/gradient-lo-fi-illustration_23-2149375747.jpg" alt="" width="500px" height="500px" />
                 </Grid>
-                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                <Grid xs={12} className='postagens'>
                 </Grid>
             </Grid>
         </>
