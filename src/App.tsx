7a449db8fa67393e8c17ecf29075a9ec731206c3
alinesoglia <alinepsoglia@gmail.com>
2023-05-18 18:09:47 -0300

    implementando a segurança no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from "./paginas/home/Home"
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './paginas/login/Login'
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario'
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -18,6 +19,7 @@ function App() {
             <Route path="/" element={<Login />} />
             <Route path="/login" element={<Login />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/cadastrousuario" element={<CadastroUsuario />} />
           </Routes>
         </div>
         <Footer />
